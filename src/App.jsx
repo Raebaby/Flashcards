b55b7519ea5105baae2d56e5cfb2616df5a3703c
Raebaby <78573429+Raebaby@users.noreply.github.com>
2023-03-08 23:49:14 -0600

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,120 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+const App = () => {
+
+const word1 = "frontal lobe"
+const word2 = "blue"
+const word3 = "green"
+const word4 = "yellow"
+const definition1 = "color of blood"
+const definition2 = "color of sea"
+const definition3 = "color of grass"
+const definition4 = "color of sun"
+
+
+const [word, setWord] = useState(word1);
+const [definition, setDefinition] = useState(definition1)
+const [side, setSide] = useState(word)
+const [count, setCount] = useState(1)
+
+
+
+
+// const flip = () =>{
+//   if(count % 2 == 0){
+//     setSide(word)
+//   }
+//   else( 
+//   setSide(definition)
+//   )
+// setCount(count + 1)
+// }
+
+
+
+
+
+const flip = () =>{
+  if(side === word){
+    setSide(definition)
+  }
+  else( 
+  setSide(word)
+  )
+setCount(count + 1)
+}
+
+
+
+const next = () => {
+  
+  if(word === word1){
+    setWord(word2)
+    setSide(word)
+    setDefinition(definition2)}
+  if(word === word2){
+    setWord(word3)
+    setDefinition(definition3)
+  }
+  if(word === word3){
+    setWord(word4)
+    setDefinition(definition4)
+  }
+  if(word === word4){
+    setWord(word1)
+    setDefinition(definition1)}
+  }
+
+
+
+// const back = () =>{
+//   if(word === word1){
+//     setWord(word4)
+//     setDefinition(definition4)}
+//   if(word === word4){
+//       setWord(word3)
+//       setDefinition(definition3)}
+//     }
+//   if(word === word3){
+//       setWord(word2)
+//       setDefinition(definition2)
+//   }
+//   if(word === word2){
+//     setWord(word1)
+//     setDefinition(definition1)
+// }
+
+
+
+
+  
   return (
+    
+
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+          <div className="header">
+            <h1>Neuroscience 101</h1>
+            <h3>Brush up on the parts of the brain!</h3>
+            <p>Number of Cards: 3</p>
+          </div>
+          <div className="card" onClick={flip}>
+            {side}
+          </div>
+          <div>
+
+          </div>
+          <div className="buttonsection">
+            <button >Back</button>
+            <button>Reset</button>
+            <button onClick={next} >Next</button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }

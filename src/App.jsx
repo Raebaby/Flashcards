43b1b73241146dd7118aa36a04480ac298d2c4c5
Raebaby <78573429+Raebaby@users.noreply.github.com>
2023-03-09 00:57:53 -0600

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import { useState } from 'react'
 import './App.css';
+import Title from './Components/Title';
 
 
 
 
 const App = () => {
 
-const word1 = "frontal lobe"
-const word2 = "blue"
-const word3 = "green"
-const word4 = "yellow"
-const definition1 = "color of blood"
-const definition2 = "color of sea"
-const definition3 = "color of grass"
-const definition4 = "color of sun"
 
+
+
+const word1 = "Frontal lobe"
+const word2 = "Occipital Lobe"
+const word3 = "Temporal Lobe"
+const word4 = "Parietal Lobe"
+const definition1 = "The frontal lobes are the largest lobes in the human brain and they are also the most common region of injury in traumatic brain injury. "
+const definition2 = "The occipital lobes sit at the back of the head and are responsible for visual perception, including colour, form and motion."
+const definition3 = "The temporal lobes sit behind the ears and are the second largest lobe. They are most commonly associated with processing auditory information and with the encoding of memory."
+const definition4 = "The parietal lobe is one of the brain's major lobes, roughly located at the upper back area in the skull."
+
+const message = "Click for the next word";
+
+const cards = [
+  word1, definition1, word2, definition2, word3, definition3, word4, definition4
+]
 
 const [word, setWord] = useState(word1);
 const [definition, setDefinition] = useState(definition1)
 const [side, setSide] = useState(word)
-const [count, setCount] = useState(1)
+const [pause, setPause] = useState("")
+const [level, setLevel] = useState('medium')
 
 
 
+console.log(cards)
 
 // const flip = () =>{
 //   if(count % 2 == 0){
@@ -43,52 +54,33 @@ const flip = () =>{
   if(side === word){
     setSide(definition)
   }
-  else( 
-  setSide(word)
-  )
-setCount(count + 1)
+  else(setSide(word))
+
 }
 
 
 
 const next = () => {
-  
   if(word === word1){
     setWord(word2)
-    setSide(word)
+    setLevel("medium")
     setDefinition(definition2)}
   if(word === word2){
     setWord(word3)
+    setLevel("purple")
     setDefinition(definition3)
   }
   if(word === word3){
+    setLevel("easy")
     setWord(word4)
     setDefinition(definition4)
   }
   if(word === word4){
     setWord(word1)
-    setDefinition(definition1)}
+    setLevel("hard")
+    setDefinition(definition1)
+  } 
   }
-
-
-
-// const back = () =>{
-//   if(word === word1){
-//     setWord(word4)
-//     setDefinition(definition4)}
-//   if(word === word4){
-//       setWord(word3)
-//       setDefinition(definition3)}
-//     }
-//   if(word === word3){
-//       setWord(word2)
-//       setDefinition(definition2)
-//   }
-//   if(word === word2){
-//     setWord(word1)
-//     setDefinition(definition1)
-// }
-
 
 
 
@@ -98,21 +90,22 @@ const next = () => {
 
     <div className="App">
       <div className="container">
-          <div className="header">
-            <h1>Neuroscience 101</h1>
-            <h3>Brush up on the parts of the brain!</h3>
-            <p>Number of Cards: 3</p>
-          </div>
-          <div className="card" onClick={flip}>
-            {side}
-          </div>
+        <Title/>
+            <div></div>
+              <br/>
+            <div className={level}>
+            <div className="content" onClick={flip}>
+              {pause}
+                <br/>
+                <br/>
+              {side}
+            </div>
+            </div>
           <div>
-
           </div>
+          <br/>
           <div className="buttonsection">
-            <button >Back</button>
-            <button>Reset</button>
-            <button onClick={next} >Next</button>
+            <button onClick={next}>Next Word</button>
           </div>
       </div>
     </div>
---
+++ src/Components/Title.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Title = () => {
+
+    return(
+        <>
+        <div className="header">
+            <h1>Neuroscience 101</h1>
+            <h3>Brush up on the parts of the brain!</h3>
+            <p>Click the card to flip.</p>
+            <p>Number of Cards: 4</p>
+        </div>
+        </>
+    )
+
+
+}
+
+export default Title
